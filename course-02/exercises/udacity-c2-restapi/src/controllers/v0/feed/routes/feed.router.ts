8225e238ec5cfcb0ec594b92cb7a9496d703dfb2
Charles Ma <1995charles@gmail.com>
2020-05-23 16:39:53 -0400

    fix deployment issue

--- course-02/exercises/udacity-c2-restapi/src/controllers/v0/feed/routes/feed.router.ts
+++ course-02/exercises/udacity-c2-restapi/src/controllers/v0/feed/routes/feed.router.ts
@@ -34,9 +34,7 @@ router.patch('/:id',
     requireAuth,
     async (req: Request, res: Response) => {
         //@TODO try it yourself
-        res.send(500).send("not implemented")
-
-        let { id } = req.params;
+        res.send(500).send("not implemented");
     });
 
 
